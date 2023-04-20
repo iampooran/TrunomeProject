0258f332aa18242bfc213f80730418b84781dec0
iampooran <pooransen50@gmail.com>
2023-04-20 17:30:52 +0530

    OTP is getting

--- src/Screens/LoginScreen.tsx
+++ src/Screens/LoginScreen.tsx
@@ -1,5 +1,5 @@
 import {View, Image, TextInput} from 'react-native';
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {styles} from './style';
 import {WelcomeScreen, facebookLogo, googleLogo} from '../assets/images';
 import {Text} from '@react-native-material/core';
@@ -13,6 +13,7 @@ import {
   statusCodes,
 } from '@react-native-google-signin/google-signin';
 import {routes} from '../utils/constents/routes';
+import auth from '@react-native-firebase/auth';
 
 const LoginScreen = ({navigation}) => {
   const handleLogin = async () => {
@@ -38,9 +39,46 @@ const LoginScreen = ({navigation}) => {
     }
   };
 
+  const [confirm, setConfirm] = useState(null);
+  const [code, setCode] = useState('');
+
+  function onAuthStateChanged(user: any) {
+    if (user) {
+      // Some Android devices can automatically process the verification code (OTP) message, and the user would NOT need to enter the code.
+      // Actually, if he/she tries to enter it, he/she will get an error message because the code was already used in the background.
+      // In this function, make sure you hide the component(s) for entering the code and/or navigate away from this screen.
+      // It is also recommended to display a message to the user informing him/her that he/she has successfully logged in.
+    }
+  }
+
   useEffect(() => {
     GoogleSignin.configure({});
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
   }, []);
+
+  // Handle the button press
+  async function signInWithPhoneNumber(phoneNumber: any) {
+    const confirmation = await auth().signInWithPhoneNumber(phoneNumber);
+    setConfirm(confirmation);
+  }
+
+  async function confirmCode() {
+    try {
+      await confirm.confirm(code);
+    } catch (error) {
+      console.log('Invalid code.');
+    }
+  }
+
+  if (!confirm) {
+    return (
+      <Button
+        title="Phone Number Sign In"
+        onPress={() => signInWithPhoneNumber('+91 7340557701')}
+      />
+    );
+  }
   return (
     <View style={styles.homeContainer}>
       <Image source={WelcomeScreen} style={styles.vectorImage} />
@@ -52,6 +90,8 @@ const LoginScreen = ({navigation}) => {
         <Box style={styles.phoneNumberBox2}>
           <Text style={styles.phoneNumber}>Phone Number</Text>
           <TextInput
+            value={code}
+            onChangeText={text => setCode(text)}
             placeholderTextColor={colors.placeholderColorPhoneNumber}
             placeholder={placeholder.phoneNumberInput}
             style={styles.phoneNumberInput}
@@ -61,6 +101,7 @@ const LoginScreen = ({navigation}) => {
       <Box style={styles.buttonBox}>
         <Box style={styles.buttonBox2}>
           <Button
+            onPress={() => confirmCode()}
             title={buttonTitle.sendOTP}
             style={styles.welcomeScreenButton}
           />
