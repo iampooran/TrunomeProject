3b1ee7cc0356ce919c990bc67e2b70e9caae8c74
iampooran <pooransen50@gmail.com>
2023-04-20 14:55:25 +0530

    Sign in prompt working

--- src/Screens/Authenticated/Authenticated.tsx
+++ src/Screens/Authenticated/Authenticated.tsx
@@ -1,10 +1,32 @@
-import {View, Text} from 'react-native';
+import {View, Button} from 'react-native';
 import React from 'react';
+import {
+  GoogleSignin,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
 
 const Authenticated = () => {
+  const signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      await GoogleSignin.signOut();
+      const userInfo = await GoogleSignin.signIn();
+      console.log(userInfo);
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+    }
+  };
   return (
     <View>
-      <Text>Authenticated</Text>
+      <Button onPress={handleLogin}>Authenticated</Button>
     </View>
   );
 };
