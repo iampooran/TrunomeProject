6f49850eec6baa4075a711249bd3721facf8d5a3
iampooran <pooransen50@gmail.com>
2023-04-20 18:18:44 +0530

    Bug Fix

--- App.tsx
+++ App.tsx
@@ -6,7 +6,6 @@ import 'react-native-gesture-handler';
 import LoginScreen from './src/Screens/LoginScreen';
 import Authenticated from './src/Screens/Authenticated/Authenticated';
 import {routes} from './src/utils/constents/routes';
-import VerifyOTP from './src/Screens/VerifyOTP/VerifyOTP';
 
 const Stack = createStackNavigator();
 
@@ -16,7 +15,6 @@ const App = () => {
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name={routes.login} component={LoginScreen} />
         <Stack.Screen name={routes.authenticated} component={Authenticated} />
-        <Stack.Screen name={routes.verifyotp} component={VerifyOTP} />
       </Stack.Navigator>
     </NavigationContainer>
   );
